5f8e52363f246aa63a6926d16c8ab1b3313bb26e
jaztorres <jazmin.torres@myma.mx>
2025-08-07 16:15:27 -0600

    Cuenta

--- src/app/pages/cuenta/cuenta.component.ts
+++ src/app/pages/cuenta/cuenta.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { HeaderComponent } from '../../shared/header/header.component';
+import { FormsModule} from '@angular/forms'
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cuenta',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent,CommonModule, FormsModule],
   templateUrl: './cuenta.component.html',
   styles: ``
 })
 export class CuentaComponent {
 
+@Input() isLogin: boolean = false;
+  model = {
+    fullName: '',
+    email: '',
+    password: '',
+    address: '',
+    state: '',
+    phone: '',
+    acceptTerms: false
+  };
 }
