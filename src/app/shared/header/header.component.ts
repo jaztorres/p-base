c1d6780d12b68eac24636d837bee05d2a85fa63a
jaztorres <jazmin.torres@myma.mx>
2025-08-13 21:36:10 -0600

    update

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
+ 
   templateUrl: './header.component.html',
   styles: ``
 })
 export class HeaderComponent {
+
+ // Puedes manejar lógica como el estado de autenticación aquí.
+  isLoggedIn = false;
+  
+  // Definimos la estructura de nuestros enlaces.
+  // Esto hace que el menú sea dinámico y fácil de mantener.
+  navLinks = [
+    { label: 'Mecánica', path: '/home', fragment:'meca'},
+    { label: 'Registro', path: '/registro' },
+    { label: 'Premios', path: '/home', fragment:'premio'},
+    { label: 'Ganadores', path: '/home' , fragment:'gana' }
+  ];
+
 
 }
